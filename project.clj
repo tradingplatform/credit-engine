65b63f9707794743dda488c2970a7250a032d448
James Squires <james.squires@gmail.com>
2014-04-06 21:08:36 -0400

    project structure

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :main ^:skip-aot credit-engine.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}}
+  :global-vars {*warn-on-reflection* true})
